795771e1136345e492fe2bd5474072989a941a9c
verébpapagáj <78073686+alexdeme1117@users.noreply.github.com>
2023-09-25 09:19:43 +0100

    new

--- menu.js
+++ menu.js
@@ -4,11 +4,15 @@ request.open("GET", "./menu.xml", false);
 request.send();
 var xml = request.responseXML;
 var foods = xml.getElementsByTagName("food");
+var dlist = document.getElementById("pizzak");
 for(var i = 0; i < foods.length; i++) {
 	var food = foods[i];
 	var names = food.getElementsByTagName("name");
 	var descs = food.getElementsByTagName("description");
 	for(var j = 0; j < names.length; j++) {
+		let opt = document.createElement("option");
+		opt.value = names[j].textContent;
+		dlist.appendChild(opt);
 		document.write("<h1 style='margin-top:" + j + "em'>" + names[j].textContent + "</h1>");
 		document.write("<h3>" + descs[j].childNodes[0].nodeValue + "</h3>");
 	}
